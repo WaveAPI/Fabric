139d15db9ad27622babe9ec750ffcb69507e37f5
nullBlade <stepan.gaidukevich@gmail.com>
2023-03-12 12:17:55 +0200

    Start of entities. (forgot vector3)

---
+++ src/main/java/org/waveapi/api/math/Vector3.java
@@ -0,0 +1,73 @@
+package org.waveapi.api.math;
+
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.math.Vec3f;
+
+public class Vector3 {
+
+    public Vector3(Vec3d v) {
+        this.v = v;
+    }
+
+    public Vector3(double x, double y, double z) {
+        this.v = new Vec3d(x, y, z);
+    }
+
+    public Vec3d v;
+    public Vector3 setX(double x) {
+        return new Vector3(new Vec3d(x, v.y, v.z));
+    }
+    public Vector3 setY(double y) {
+        return new Vector3(new Vec3d(v.x, y, v.z));
+    }
+    public Vector3 setZ(double z) {
+        return new Vector3(new Vec3d(v.x, v.y, z));
+    }
+
+
+    public double getX() {
+        return v.x;
+    }
+    public double getY() {
+        return v.y;
+    }
+    public double getZ() {
+        return v.z;
+    }
+
+    public Vector3 addX(double x) {
+        return new Vector3(v.add(x, 0, 0));
+    }
+    public Vector3 addY(double y) {
+        return new Vector3(v.add(0, y, 0));
+    }
+    public Vector3 addZ(double z) {
+        return new Vector3(v.add(0, 0, z));
+    }
+
+    public Vector3 add(Vector3 vector) {
+        return new Vector3(v.add(vector.v));
+    }
+    public Vector3 add(double x, double y, double z) {
+        return new Vector3(v.add(x, y, z));
+    }
+
+    public Vector3 subtract(double x, double y, double z) {
+        return new Vector3(v.subtract(x, y, z));
+    }
+
+    public double distanceTo(Vector3 vector3) {
+        return v.distanceTo(vector3.v);
+    }
+
+    public Vector3 rotateX(float angle) {
+        return new Vector3(v.rotateX(angle));
+    }
+    public Vector3 rotateY(float angle) {
+        return new Vector3(v.rotateY(angle));
+    }
+    public Vector3 rotateZ(float angle) {
+        return new Vector3(v.rotateZ(angle));
+    }
+
+}
