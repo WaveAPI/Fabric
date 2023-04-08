1754ee1a9e19f53710516f44e599217ea2821e7b
nullBlade <stepan.gaidukevich@gmail.com>
2023-04-08 10:48:00 +0300

    Translation fixes

--- src/main/java/org/waveapi/api/misc/TranslatedText.java
+++ src/main/java/org/waveapi/api/misc/TranslatedText.java
@@ -3,20 +3,20 @@ package org.waveapi.api.misc;
 import org.waveapi.api.WaveMod;
 import org.waveapi.content.resources.LangManager;
 
-public class TranslatedText extends Text {
+import static org.waveapi.Main.bake;
 
-    private static int id = 0;
+public class TranslatedText extends Text {
     private final WaveMod mod;
-    private String key;
+    private final String key;
 
     public TranslatedText(String name, WaveMod mod) {
-        super(net.minecraft.text.Text.translatable("translatable." + id + ".waveAPI_text." + mod.name + "." + name));
-        key = "translatable." + id + ".waveAPI_text." + mod.name + "." + name;
-        id += 1;
+        super(net.minecraft.text.Text.translatable("translatable.waveAPI_text." + mod.name + "." + name));
+        key = "translatable.waveAPI_text." + mod.name + "." + name;
         this.mod = mod;
     }
 
     public TranslatedText addTranslation(String language, String translation) {
+        if (!bake)return this;
         LangManager.addTranslation(mod.name, language, key, translation);
         return this;
     }
